8cca7d287deb9c67c19a1ab8eeee6c10d9d84da8
Artooorcheck <amin3010@mail.ru>
2023-08-13 20:30:24 +0300

    .

---
+++ words and pictures/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace words_and_pictures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(365, 426);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(520, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(268, 308);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ words and pictures/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace words_and_pictures
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, ListViewItem> _namesDictionary;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _namesDictionary = new Dictionary<string, ListViewItem>();
+            listView1.MultiSelect = false;
+            InitView();
+            listBox1.SelectedIndexChanged += OnItemSelected;
+        }
+
+        private void InitView()
+        {
+            var file = new StreamWriter("./var.txt");
+            var fileNames = Directory.GetFiles("./Files");
+            List<string> names = new List<string>();
+            for (int i = 0; i<fileNames.Length; i++)
+            {
+                var name = fileNames[i];
+                using (var stream = new FileStream(name, FileMode.Open))
+                {
+                    var image = Image.FromStream(stream);
+                    imageList.Images.Add(image);
+                    var pathArray = name.Split('\\');
+                    name = pathArray[pathArray.Length - 1].Split('.')[0];
+                    names.Add(name);
+                ListViewItem item = new ListViewItem();
+                item.ImageIndex = i;
+                listView1.Items.Add(item);
+                listBox1.Items.Add(names[i]);
+                    _namesDictionary[name] = item;
+                }
+            }
+            listView1.View = View.LargeIcon;
+            imageList.ImageSize = new Size(128, 128);
+            listView1.LargeImageList = imageList;
+            var rand = new Random();
+            names = names.OrderBy(a => rand.Next()).ToList();
+
+            for(int number = 0; number < imageList.Images.Count; number++)
+            {
+            }
+        }
+
+        private void OnItemSelected(object o, EventArgs e)
+        {
+            int nameIndex = listBox1.SelectedIndex;
+            try
+            {
+                var name = listBox1.Items[nameIndex].ToString();
+                var imageIndex = listView1.SelectedItems;
+                if (imageIndex.Contains(_namesDictionary[name]))
+                {
+                    foreach (ListViewItem item in imageIndex)
+                    {
+                        listView1.Items.Remove(item);
+                    }
+                    listBox1.Items.RemoveAt(nameIndex);
+                }
+                else
+                {
+                    listBox1.SelectedIndex = -1;
+                    listView1.SelectedIndices.Clear();
+                    
+                }
+            }
+            catch { }
+        }
+    }
+}
